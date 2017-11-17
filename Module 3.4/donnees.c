edf6647a1118571d4df76705ac06f4455d72f362
AlexandreLedru <33116767+AlexandreLedru@users.noreply.github.com>
2017-11-17 13:50:04 +0100

    Update donnees.c

--- Module 3.4/donnees.c
+++ Module 3.4/donnees.c
@@ -8,3 +8,20 @@ FILE* ouvrirFichier(void)
 
     return battements;
 }
+int init_size (FILE* battements)
+{
+    int i,sizetab=0,a;
+    int* donnees;
+    int** index[2];
+    while(a!=EOF)
+    {
+        a=fgetc(battements);
+        if (a=='\n')
+        {
+            sizetab++;
+        }
+    }
+    rewind(battements);
+    return sizetab;
+}
+
