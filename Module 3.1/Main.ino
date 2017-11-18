c99f5eb3194175e2b5036354b665c58cbdba6d54
AlexandreLedru <33116767+AlexandreLedru@users.noreply.github.com>
2017-11-18 16:31:16 +0100

    Update Main.ino

--- Module 3.1/Main.ino
+++ Module 3.1/Main.ino
@@ -1,19 +1,1 @@
-#include "cardio.h"
-#include "FreqMeasure.h"
 
-void setup() {
-  Serial.begin(9600);
-  //FreqMeasure.begin();    //initialisation du calcul de fréquence
-}
-
-void loop() {
-  int tps;
-  double pls;             // défintion des variables
-  tps = temps();          // récupération du temps
-  //pls = pouls();        // récupération de la fréquence
-  pls = random(60,90);    // simulation d'un pouls
-  Serial.print(tps);      // affichage au format CSV
-  Serial.print(";");
-  Serial.println(pls);
-
-}
